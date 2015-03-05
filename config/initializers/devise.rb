b45097b2f8cdf9279749fec8c544ce7000b9df87
Christian Cruz <christian@simpleshape.me>
2015-03-05 14:17:00 -0600

    debugging devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Confirm your registration with Blocitoff'
+  config.mailer_sender = 'cruz.dardenne@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
