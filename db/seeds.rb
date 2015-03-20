7f02e76724c08a48c2f7d90c6906a55dd7b4267b
Christian Cruz <christian@simpleshape.me>
2015-03-20 00:50:38 -0500

    Displaying time remaining

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@ require 'faker'
   #Add code to date posts
   #Add code to date posts
   #Add code to date posts
-  @user.items.create!(name: Faker::Lorem.sentence)
+  @user.items.create!(name: Faker::Lorem.sentence, created_at: Random.rand(7).days.ago)
+
 end
 items = Item.all
 
